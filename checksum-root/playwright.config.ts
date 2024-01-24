b762745ef6a1a762904f384d9e1ede1238f3ea83
gal-checksum <114691635+gal-checksum@users.noreply.github.com>
2024-01-24 14:47:16 -0800

    Update playwright.config.ts

--- checksum-root/playwright.config.ts
+++ checksum-root/playwright.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: process.env.CI
-    ? [["html", { open: "never", outputFolder: "test-results" }], ["line"]]
+    ? [["html", { open: "never"}], ["line"]], ["line"]]
     : "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
